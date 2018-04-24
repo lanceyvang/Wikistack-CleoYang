0dda56ba213a6385aae87102956b1c170daa8eef
Cleo L <cleoFA@Cleos-MacBook-Air.local>
2018-04-24 17:18:41 -0400

    second

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 app = express();
 const morgan = require('morgan');
+const parser = require('body-parser');
+const nunjucks = require('nunjucks');
+
+const env = nunjucks.configure('view', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.listen('3000', console.log('Server is running.'));
