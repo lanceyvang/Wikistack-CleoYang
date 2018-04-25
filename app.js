b2e32c10dfa57ec852ae23fd86fceb0ec758c6ec
Yang Vang <lanceyvang@gmail.com>
2018-04-25 10:56:01 -0400

    synced table

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const path = require('path');
 const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://localhost:5432/wikistack");
-const pg = require("pg");
 const models = require('./models');
 
 
@@ -18,7 +16,7 @@ app.engine('html', nunjucks.render);
 //Static MiddleWare
 app.use(express.static(path.join(__dirname, "/public")));
 
-//SYNC
+// SYNC
 models.User.sync()
   .then(function() {
     console.log("User table created!");
@@ -31,16 +29,3 @@ models.User.sync()
     });
   })
   .catch(console.error.bind(console));
-
-// models.db
-//   .sync()
-//   .then(function() {
-//     console.log("All tables created!");
-//     app.listen(3000, function() {
-//       console.log("Server is listening on port 3000!");
-//     });
-//   })
-//   .catch(console.error.bind(console));
-
-//Runs Server
-app.listen('3000', console.log('Server is running.'));
